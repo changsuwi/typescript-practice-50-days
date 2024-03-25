bac30654fd2576629d88583af297541d46ea940b
Vicharm <vicharm.chang@ailabs.tw>
2024-03-25 22:13:26 +0800

    test: add test case in day-5 dropWhile

--- src/5-dropWhile/dropWhile.spec.ts
+++ src/5-dropWhile/dropWhile.spec.ts
@@ -1,12 +1,24 @@
-import { describe, expect, test } from 'vitest';
-import { dropWhile } from './dropWhile';
+import { describe, expect, test } from "vitest";
+import { dropWhile } from "./dropWhile";
 
-describe("dropWhile", () =>{
+describe("dropWhile", () => {
   test("should drop value < 4, return [4,5,6]", () => {
-    expect(dropWhile([1, 2, 3, 4, 5, 6], (value: any) => value < 4)).toStrictEqual([4, 5, 6])
-  })
+    expect(
+      dropWhile([1, 2, 3, 4, 5, 6], (value: any) => value < 4)
+    ).toStrictEqual([4, 5, 6]);
+  });
 
   test("should drop value < 5, return []", () => {
-    expect(dropWhile([1, 2, 3], (value: any) => value < 5)).toStrictEqual([])
-  })
-})
+    expect(dropWhile([1, 2, 3], (value: any) => value < 5)).toStrictEqual([]);
+  });
+
+  test("give empty array, should return empty array", () => {
+    expect(dropWhile([], (value: any) => value < 5)).toStrictEqual([]);
+  });
+
+  test("there is a element not fulfill predicate function in the middle, should return [6, 1, 2]", () => {
+    expect(dropWhile([0, 6, 1, 2], (value) => value < 5)).toStrictEqual([
+      6, 1, 2,
+    ]);
+  });
+});
